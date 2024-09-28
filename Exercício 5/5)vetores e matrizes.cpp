eaae079ddc294c17c388b39786d2fe2622408446
Johan <johanoliveirabta@gmail.com>
2024-09-28 18:50:01 -0300

    Update 5)vetores e matrizes.cpp

--- Exercício 5/5)vetores e matrizes.cpp
+++ Exercício 5/5)vetores e matrizes.cpp
@@ -2,48 +2,44 @@
 #include <locale.h>
 #define Total 10
 
+
 using namespace std;
+
 
 float Num[Total];
 int PMaiorNum, PMenorNum, Cont;
+
 
 int main () {
 	setlocale (LC_ALL,"Portuguese");
 	
 	for(Cont=0; Cont<Total; Cont++){
-		cout << " Digite o número da " << Cont << "° posição: ";
+		cout << " Digite o nÃºmero da " << Cont+1 << "Â° posiÃ§Ã£o: ";
 		cin >> Num[Total];
 		system("cls");
 		
-		switch(1){
-			case (Cont==0){
-				PMaiorNum = Cont;
-				PMenorNum = Cont;
-				break;
-			}
-			case Num[Cont] > Num[PMaiorNum]{
-				PMaiorNum = Cont;
-				break;
-			}
-			case Num[Cont] < Num[PMenorNum]{
-				PMenorNum = Cont;
-				break;
-			}
+		if(Cont==0){
+			PMaiorNum = Cont;
+			PMenorNum = Cont;
+		}
+		if(Num[Cont] > Num[PMaiorNum]){
+			PMaiorNum = Cont;
+		}
+		if(Num[Cont] < Num[PMenorNum]){
+			PMenorNum = Cont;
 		}
 	}
 	
 	for(Cont=0; Cont<Total; Cont++){
-		switch{
-			case 1(Num[Cont] == MaiorNum){
-				cout << " O maior número é " << MaiorNum << " na posição " << Cont+1 << endl;
-				break;
-			}
-			case 2(Num[Cont] == MenorNum){
-				cout << " O menor número é " << MenorNum << " na posição " << Cont+1;
-				break;
+		
+		if(Num[Cont] == PMaiorNum){
+			cout << " O maior nÃºmero Ã© " << PMaiorNum << " na posiÃ§Ã£o " << Cont+1 << endl;
+		}
+		else{
+			if(Num[Cont] == PMenorNum){
+				cout << " O menor nÃºmero Ã© " << PMenorNum << " na posiÃ§Ã£o " << Cont+1;
 			}
 		}
 	}
-	
 	return 0;
 }
